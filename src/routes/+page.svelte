2ba56b0e31526d9e3197852b3b4c6f81106ec661
Cody McCormick <mr.codymccormick@gmail.com>
2023-02-26 15:59:20 -0600

    converted to svelte app

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+	import Character from "./Character.svelte";
+    import Header from "./Header.svelte"
+</script>
+<Header/>
+<Character/>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+	<body>
+        <h1>Character Generator</h1>
+
+		<style>
+			h1 {
+				text-align: center;
+                color:red;
+			};
+		</style>
+	</body>
+</html>
