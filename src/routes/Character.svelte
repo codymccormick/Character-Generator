b98883db4c42554d822eebb491685667cda6dcc3
Cody McCormick <mr.codymccormick@gmail.com>
2023-02-28 10:03:30 -0600

    added Occupations

--- src/routes/Character.svelte
+++ src/routes/Character.svelte
@@ -5,46 +5,12 @@
 		<button id="generate-character-button">Generate Character</button>
 
 		<table class="character" id="character">
-			<tr>
-				<th>Name</th>
-				<td />
-			</tr>
-			<tr>
-				<th>Alignment</th>
-				<td />
-			</tr>
-			<tr>
-				<th>Race</th>
-				<td />
-			</tr>
-			<tr>
-				<th>Class</th>
-				<td />
-			</tr>
-			<tr>
-				<th>Strength</th>
-				<td />
-			</tr>
-			<tr>
-				<th>Dexterity</th>
-				<td />
-			</tr>
-			<tr>
-				<th>Constitution</th>
-				<td />
-			</tr>
-			<tr>
-				<th>Intelligence</th>
-				<td />
-			</tr>
-			<tr>
-				<th>Wisdom</th>
-				<td />
-			</tr>
-			<tr>
-				<th>Charisma</th>
-				<td />
-			</tr>
+			{#each ['Name', 'Alignment', 'Race', 'Class', 'Occupation', 'Strength', 'Dexterity', 'Constitution', 'Intelligence', 'Wisdom', 'Charisma'] as prop}
+				<tr>
+					<th>{prop}</th>
+					<td />
+				</tr>
+			{/each}
 		</table>
 
 		<script>
@@ -53,37 +19,45 @@
 			generateCharacterButton.addEventListener('click', generateCharacter);
 
 			// Define names
-			const NAMES = [
+			const names = [
 				'Aldric',
 				'Briar',
 				'Cassius',
-				'Darian',
 				'Elden',
-				'Finnian',
-				'Galen',
-				'Hadrian',
-				'Isen',
-				'Jareth',
 				'Kael',
-				'Lirien',
 				'Marius',
-				'Niamh',
 				'Orion',
-				'Pryderi',
-				'Quinlan',
 				'Riven',
 				'Sebastian',
-				'Taliesin',
-				'Urien',
-				'Valerian',
-				'Wren',
-				'Xander',
-				'Yorick',
 				'Zephyr'
 			];
 
+			const occupations = [
+				'Baker',
+				'Blacksmith',
+				'Barber',
+				'Guard',
+				'Butcher',
+				'Chef',
+				'Mason',
+				'Fletcher',
+				'Cobbler'
+			];
+
+			const alignments = [
+				'Lawful Good',
+				'Neutral Good',
+				'Chaotic Good',
+				'Lawful Neutral',
+				'True Neutral',
+				'Chaotic Neutral',
+				'Lawful Evil',
+				'Neutral Evil',
+				'Chaotic Evil'
+			];
+
 			//Define races
-			const RACES = [
+			const races = [
 				{ name: 'Dwarf', str: 2, dex: 0, con: 2, int: -2, wis: 0, cha: -2 },
 				{ name: 'Elf', str: 0, dex: 2, con: -2, int: 2, wis: 0, cha: 0 },
 				{ name: 'Halfling', str: -2, dex: 2, con: 2, int: 0, wis: 0, cha: 0 },
@@ -96,7 +70,7 @@
 			];
 
 			//Define classes
-			const CLASSES = [
+			const classes = [
 				{ name: 'Barbarian', hitDie: 12, str: 2, dex: 0, con: 2, int: -2, wis: 0, cha: -2 },
 				{ name: 'Bard', hitDie: 8, str: 0, dex: 2, con: 0, int: 2, wis: 0, cha: 2 },
 				{ name: 'Cleric', hitDie: 8, str: 0, dex: 0, con: 2, int: 0, wis: 2, cha: 0 },
@@ -111,48 +85,29 @@
 				{ name: 'Wizard', hitDie: 6, str: -2, dex: 0, con: 0, int: 2, wis: 0, cha: 0 }
 			];
 
-			function generateAlignment() {
-				const alignments = [
-					'Lawful Good',
-					'Neutral Good',
-					'Chaotic Good',
-					'Lawful Neutral',
-					'True Neutral',
-					'Chaotic Neutral',
-					'Lawful Evil',
-					'Neutral Evil',
-					'Chaotic Evil'
-				];
-				return alignments[Math.floor(Math.random() * alignments.length)];
-			}
-
-			/* // Define a function to generate a random integer in a given range
-			function getRandomInt(min, max) {
-				return Math.floor(Math.random() * (max - min + 1)) + min;
-			}
-
 			function rollAbilityScores() {
-  				const rolls = [];
-  				for (let i = 0; i < 6; i++) {
-   				 const diceRolls = Array.from({ length: 4 }, () => Math.floor(Math.random() * 6) + 1);
-    			const lowestRoll = Math.min(...diceRolls);
-    			const abilityScore = diceRolls.reduce((total, roll) => total + roll, 0) - lowestRoll;
-    			rolls.push(abilityScore);
-  				}
-  				return rolls;
+				const rolls = [];
+				for (let i = 0; i < 6; i++) {
+					const diceRolls = Array.from({ length: 4 }, () => Math.floor(Math.random() * 6) + 1);
+					const lowestRoll = Math.min(...diceRolls);
+					const abilityScore = diceRolls.reduce((total, roll) => total + roll, 0) - lowestRoll;
+					rolls.push(abilityScore);
+				}
+				return rolls;
 			}
 
 			function getRandomItemFromArray(array) {
-  				const randomIndex = Math.floor(Math.random() * array.length);
-  				return array[randomIndex];
-			} */
+				const randomIndex = Math.floor(Math.random() * array.length);
+				return array[randomIndex];
+			}
 
 			// Define a function to generate a character
 			function generateCharacter() {
-				const name = NAMES[Math.floor(Math.random() * NAMES.length)];
-				const race = RACES[Math.floor(Math.random() * Object.keys(RACES).length)];
-				const characterClass = CLASSES[Math.floor(Math.random() * CLASSES.length)];
-				const alignment = generateAlignment();
+				const name = getRandomItemFromArray(names);
+				const race = getRandomItemFromArray(races);
+				const characterClass = getRandomItemFromArray(classes);
+				const occupation = getRandomItemFromArray(occupations);
+				const alignment = getRandomItemFromArray(alignments);
 
 				const attributes = ['str', 'dex', 'con', 'int', 'wis', 'cha'].map((attr) => {
 					const rolls = [...new Array(4)].map(() => Math.floor(Math.random() * 6) + 1);
@@ -160,22 +115,23 @@
 						.sort((a, b) => a - b)
 						.slice(1)
 						.reduce((total, num) => total + num, 0);
-					const raceModifier = RACES[attr] || 0;
+					const raceModifier = races[attr] || 0;
 					const classModifier = characterClass[attr] || 0;
 					const value = base + raceModifier + classModifier;
 					return Math.min(20, Math.max(1, value)); // Ensure the value is between 1 and 20
 				});
 
-				const [nameCell, alignmentCell, raceCell, classCell, ...statCells] = document
+				const [nameCell, alignmentCell, raceCell, classCell, occupationCell, ...statCells] = document
 					.getElementById('character')
 					.getElementsByTagName('td');
-				const temp = [name, alignment, race.name, characterClass.name, ...attributes];
+				const temp = [name, alignment, race.name, characterClass.name, occupation, ...attributes];
 				nameCell.textContent = temp[0];
 				alignmentCell.textContent = temp[1];
 				raceCell.textContent = temp[2];
 				classCell.textContent = temp[3];
+				occupationCell.textContent = temp[4]
 				for (let i = 0; i < statCells.length; i++) {
-					statCells[i].textContent = temp[i + 4];
+					statCells[i].textContent = temp[i + 5];
 				}
 			}
 		</script>
