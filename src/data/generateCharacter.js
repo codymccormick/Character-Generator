90f0579bd73f9adb048d1a0848acc3cb9a5bc015
Cody McCormick <mr.codymccormick@gmail.com>
2023-03-21 09:30:41 -0500

    Converted Character creation function to a class

--- src/data/generateCharacter.js
+++ src/data/generateCharacter.js
@@ -1,59 +1,71 @@
-import { genders, classes, alignments, occupations, backgrounds } from './characterGeneration/data';
-import { rollStat, generateAge } from '../helpers/helper';
-import faker from 'faker';
-import { characterStore } from './store';
 import {
-	generateRace,
-	generateBirthplace,
-	generateChildhoodEnvironment
-} from './characterGeneration/origin';
+  genders,
+  classes,
+  alignments,
+  occupations,
+  backgrounds,
+} from "./characterGeneration/data";
+import { rollStat, generateAge } from "../helpers/helper";
+import faker from "faker";
+import { characterStore } from "./store";
 import {
-	generateSiblings,
-	rollCaretakerStatus,
-	generateFamilyBackground,
-	generateCaretakerOrigin
-} from './characterGeneration/family';
-import { generateRandomItemFromObject, getRandomItemFromArray } from '../helpers/random';
+  generateRace,
+  generateBirthplace,
+  generateChildhoodEnvironment,
+} from "./characterGeneration/origin";
+import {
+  generateSiblings,
+  rollCaretakerStatus,
+  generateFamilyBackground,
+  generateCaretakerOrigin,
+} from "./characterGeneration/family";
+import {
+  generateRandomItemFromObject,
+  getRandomItemFromArray,
+} from "../helpers/random";
+
+class Character {
+  constructor() {
+    this.name = faker.name.findName();
+    this.age = generateAge();
+    this.race = generateRace();
+    this.gender = getRandomItemFromArray(genders);
+    this.class = getRandomItemFromArray(classes);
+    this.alignment = getRandomItemFromArray(alignments);
+    Object.assign(this, generateBirthplace());
+    Object.assign(this, generateChildhoodEnvironment());
+    Object.assign(this, generateCaretakerOrigin());
+    this.caretakerStatus = rollCaretakerStatus();
+    Object.assign(this, generateFamilyBackground());
+    this.siblings = generateSiblings();
+    this.strength = rollStat();
+    this.dexterity = rollStat();
+    this.constitution = rollStat();
+    this.intelligence = rollStat();
+    this.wisdom = rollStat();
+    this.charisma = rollStat();
+
+    const { background, backgroundReason } = generateRandomItemFromObject(
+      backgrounds,
+      "background",
+      "backgroundReason"
+    );
+    const { occupation, occupationReason } = generateRandomItemFromObject(
+      occupations,
+      "occupation",
+      "occupationReason"
+    );
+
+    this.occupation = occupation;
+    this.occupationReason = occupationReason;
+    this.background = background;
+    this.backgroundReason = backgroundReason;
+  }
+}
 
 export const generateCharacter = () => {
-	// Generate character background and occupation
-	const { background, backgroundReason } = generateRandomItemFromObject(
-		backgrounds,
-		'background',
-		'backgroundReason'
-	);
-	const { occupation, occupationReason } = generateRandomItemFromObject(
-		occupations,
-		'occupation',
-		'occupationReason'
-	);
-
-	// Create the character object
-	let character = {
-		name: faker.name.findName(),
-		age: generateAge(),
-		race: generateRace(),
-		gender: getRandomItemFromArray(genders),
-		class: getRandomItemFromArray(classes),
-		alignment: getRandomItemFromArray(alignments),
-		...generateBirthplace(),
-		...generateChildhoodEnvironment(),
-		...generateCaretakerOrigin(),
-		caretakerStatus: rollCaretakerStatus(),
-		...generateFamilyBackground(),
-		siblings: generateSiblings(),
-		occupation,
-		occupationReason,
-		background,
-		backgroundReason,
-		strength: rollStat(),
-		dexterity: rollStat(),
-		constitution: rollStat(),
-		intelligence: rollStat(),
-		wisdom: rollStat(),
-		charisma: rollStat()
-	};
+	const character = new Character();
 	characterStore.set(character);
 	console.log(character);
 	return character;
-};
+  };
