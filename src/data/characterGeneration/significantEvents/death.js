8ca45378597c1c170d47229027bd18a470789fb1
Cody McCormick <mr.codymccormick@gmail.com>
2023-03-28 12:24:18 -0500

    Simplififed death and misfortune functions

--- src/data/characterGeneration/significantEvents/death.js
+++ src/data/characterGeneration/significantEvents/death.js
@@ -1,52 +1,14 @@
-// The rollDeath function determines death based on a random roll
 export const rollDeath = () => {
 	const roll = Math.floor(Math.random() * 12) + 1;
-	let result;
-
-	switch (roll) {
-		case 1:
-		case 2:
-			result = {
-				death: 'Warfare',
-				description: 'An event ranging from a raid to a siege caused death'
-			};
-			break;
-		case 3:
-		case 4:
-		case 5:
-			result = {
-				death: 'Disease ',
-				description: 'Anything from a simple cold to a Pox caused death'
-			};
-			break;
-		case 6:
-		case 7:
-			result = {
-				death: 'Accident',
-				description: 'Any number of random events, from a mule kicking at the wrong time to a fire'
-			};
-			break;
-		case 8:
-		case 9:
-			result = {
-				death: 'Murdered',
-				description: `${roll >= 10 ? 'You know who did it' : 'You do not know who did it'}`
-			};
-			break;
-		case 10:
-		case 11:
-			result = {
-				death: 'Unknown',
-				description: 'Found dead under mysterious circumstances'
-			};
-			break;
-		case 12:
-			result = {
-				death: 'Murdered by You',
-				description: 'You did the killing '
-			};
-			break;
-	}
-
-	return result;
-};
+	const outcomes = [
+	  { range: [1, 2], death: 'Warfare', description: 'An event ranging from a raid to a siege caused death' },
+	  { range: [3, 4, 5], death: 'Disease', description: 'Anything from a simple cold to a Pox caused death' },
+	  { range: [6, 7], death: 'Accident', description: 'Any number of random events, from a mule kicking at the wrong time to a fire' },
+	  { range: [8, 9], death: 'Murdered', description: `${roll >= 10 ? 'You know who did it' : 'You do not know who did it'}` },
+	  { range: [10, 11], death: 'Unknown', description: 'Found dead under mysterious circumstances' },
+	  { range: [12], death: 'Murdered by You', description: 'You did the killing' },
+	];
+  
+	return outcomes.find(outcome => outcome.range.includes(roll));
+  };
+  
--- src/data/characterGeneration/significantEvents/misfortune.js
+++ src/data/characterGeneration/significantEvents/misfortune.js
@@ -1,56 +1,54 @@
 export const rollMisfortune = () => {
 	const roll = Math.floor(Math.random() * 12) + 1;
-	let result;
+	const outcomes = [
+		{
+			range: [1, 2],
+			misfortune: "Cult",
+			description: "Became involved with a religious cult",
+		},
+		{
+			range: [3, 4],
+			misfortune: "Addiction",
+			description: "Be it drugs, alcohol or gambling",
+		},
+		{
+			range: [5, 6],
+			misfortune: "Crippled",
+			description:
+				"Accident, random encounter or worse causing a partial or complete incapacitation",
+		},
+		{
+			range: [7],
+			misfortune: "Cursed",
+			description: "Lycanthropy, vampirism or other such",
+		},
+		{
+			range: [8],
+			misfortune: "Taken",
+			description: "Be it by roaming barbarians, organized crime or slavers",
+		},
+		{
+			range: [9],
+			misfortune: "Indentured Servant",
+			description:
+				"Bad deals or bad luck left a large debt to be paid off by labor, or outright slavery",
+		},
+		{
+			range: [10],
+			misfortune: "Bankruptcy",
+			description: "Responsible, directly or indirectly, for the loss of all gold and lands",
+		},
+		{
+			range: [11],
+			misfortune: "Crazy",
+			description: "Some lamentable situation caused complete mental breakdown",
+		},
+		{
+			range: [12],
+			misfortune: "Prison",
+			description: "Situations led to long term incarceration",
+		},
+	];
 
-	switch (roll) {
-		case 1:
-		case 2:
-			result = { misfortune: 'Cult', description: 'Became involved with a religious cult' };
-			break;
-		case 3:
-		case 4:
-			result = { misfortune: 'Addiction', description: 'Be it drugs, alcohol or gambling' };
-			break;
-		case 5:
-		case 6:
-			result = {
-				misfortune: 'Crippled',
-				description:
-					'Accident, random encounter or worse causing a partial or complete incapacitation'
-			};
-			break;
-		case 7:
-			result = { misfortune: 'Cursed', description: 'Lycanthropy, vampirism or other such' };
-			break;
-		case 8:
-			result = {
-				misfortune: 'Taken',
-				description: 'Be it by roaming barbarians, organized crime or slavers'
-			};
-			break;
-		case 9:
-			result = {
-				misfortune: 'Indentured Servant',
-				description:
-					'Bad deals or bad luck left a large debt to be paid off by labor, or outright slavery'
-			};
-			break;
-		case 10:
-			result = {
-				misfortune: 'Bankruptcy',
-				description: 'Responsible, directly or indirectly, for the loss of all gold and lands'
-			};
-			break;
-		case 11:
-			result = {
-				misfortune: 'Crazy',
-				description: 'Some lamentable situation caused complete mental breakdown'
-			};
-			break;
-		case 12:
-			result = { misfortune: 'Prison', description: 'Situations led to long term incarceration' };
-			break;
-	}
-
-	return result;
+	return outcomes.find((outcome) => outcome.range.includes(roll));
 };
