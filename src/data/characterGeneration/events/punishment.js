85eff3e49aea7da12202b0afbfe105ea51004319
Cody McCormick <mr.codymccormick@gmail.com>
2023-04-04 09:39:35 -0500

    Added punishment event

---
+++ src/data/characterGeneration/events/punishment.js
@@ -0,0 +1,28 @@
+import { getRandomItemFromArray } from "../../../helpers/random";
+
+export const generatePunishmentEvent = () => {
+    return getRandomItemFromArray(punishmentEvents);
+}
+
+export const punishmentEvents = [
+    {
+        title: "Exonerated",
+        type: "Punishment Event",
+        description: "You did not commit the crime and were exonerated after being accused.",
+    },
+    {
+        title: "Found Not Guilty",
+        type: "Punishment Event",
+        description: "You committed the crime or helped do so, but nonetheless the authorities found you not guilty.",
+    },
+    {
+        title: "Wanted",
+        type: "Punishment Event",
+        description: "You were nearly caught in the act. You had to flee and are wanted in the community where the crime occurred.",
+    },
+    {
+        title: "Convicted",
+        type: "Punishment Event",
+        description: "You were caught and convicted. You spent time in jail, chained to an oar, or performing hard labor. You served a sentence of 1d4 years or succeeded in escaping after that much time.",
+    },
+];
